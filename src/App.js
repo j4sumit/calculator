7a6a985c50311839ef589393ddfdb47672f8de5b
unknown <J4sumit@gmail.com>
2023-05-01 23:14:54 +0530

    Header and keypad componet created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-
+import moonIcon from "./assets/moon.png";
+import sunIcon from "./assets/sun.png";
+import Header from "./Components/Header/Header";
+import KeyPad from "./Components/KeyPad/KeyPad";
 import './App.css';
+import React, {useState} from "react";
 
 function App() {
+  const[isDarkMode, setIsDarkMode]= useState(false);
   return (
-    <div className="App">
-<h1> Calculator</h1>
+    <div className="app">
+      <div className="app_calculator">
+        <div className="app_calculator_navbar">
+          <div className="app_calculator_navbar_toggle"
+            onClick={()=> setIsDarkMode(!isDarkMode)}>
+            <div className={`app_calculator_navbar_toggle_circle ${isDarkMode ? "app_calculator_navbar_toggle_circle_active" : ""}`}/>
+
+            </div>
+          <img src={isDarkMode? moonIcon: sunIcon} alt="mode"/>
+        </div>
+<Header />
+<KeyPad/>
+      </div>
+      
     </div>
   );
 }
---
+++ src/Components/KeyPad/KeyPad.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./KeyPad.css";
+function KeyPad() {
+  return (
+    <div className='keypad'>
+        <h1>KeyPad</h1>
+        </div>
+  )
+}
+
+export default KeyPad;
